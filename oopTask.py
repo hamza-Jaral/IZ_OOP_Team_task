d9ecf7ccb114d67e9fac836daf8ba56d5b75e1eb
hamza-Jaral <hamzajarral62@gmail.com>
2021-12-19 16:44:40 +0500

    first commit

--- oopTask.py
+++ oopTask.py
@@ -1,1 +1,17 @@
+class Bike:
+    def __init__(self, color, engine, speed):
+        self.color = color
+        self.engine = engine
+        self.speed = speed
+    
+    def __str__(self):
+        return f'this is a bike of {self.color} color with engine {self.engine}'
+    
+    ## hassan has to write his code between these two commented lines##
+    ##hasan code start
+    # hasan code end
 
+
+    #arslan has to write his code between these two commented lines
+    # arslan code start
+    # arslan code end
